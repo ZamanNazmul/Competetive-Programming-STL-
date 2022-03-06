8690ba3bd3fc62ba0e8d4e5fb0d0caff9e362739
ZamanNazmul <95521785+ZamanNazmul@users.noreply.github.com>
2022-03-06 17:04:30 +0600

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int n)
+{
+
+	int factorial=1;
+	for(int i=2;i<=n;i++)
+	{
+		 factorial=factorial*i;
+	}
+	return factorial;
+}
+
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fact(n);
+}
